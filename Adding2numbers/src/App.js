14201d79a2db24acfe5dbf499e000b47f2517d8f
bavijothi9 <132434261+bavijothi9@users.noreply.github.com>
2023-12-05 16:22:22 +0530

    Add files via upload

---
+++ Adding2numbers/src/App.js
@@ -0,0 +1,57 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import counter from "./counter";
+// function App() {
+//   return (
+//     <div className = "App">
+//       <counter/>
+//     </div>
+//   );
+// };
+// export default App;
+
+import Add from "./Add";
+function App() {
+  return (
+    <div className = "App">
+      <Add/>
+    </div>
+  );
+};
+export default App;
+
+// import Form from "./Form";
+// function App() {
+//   return (
+//     <div className = "App">
+//       <Form/>
+//     </div>
+//   );
+// };
+// export default App;
+
